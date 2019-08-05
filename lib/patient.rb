20f5795a81a97a30961a8609bc57513ee2cf918a
jessi ortega <ortegabx05@gmail.com>
2019-08-05 22:46:20 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,17 @@
 class Patient
   attr_accessor :name 
   
+  @@all = []
+  
   def initialize(name)
     @name=name
-  
+   @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
   end
   
    def new_appointment(doctor, date)
