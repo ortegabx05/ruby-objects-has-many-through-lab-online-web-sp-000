cf8f6caf31a349ee65d5133b94b58207ed5535a9
jessi ortega <ortegabx05@gmail.com>
2019-08-05 22:45:32 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -3,13 +3,8 @@ class Patient
   
   def initialize(name)
     @name=name
-   @@all << self
+  
   end
-
-  def self.all
-    @@all
-  end
-
   
    def new_appointment(doctor, date)
     Appointment.new(date, self, doctor)
