979fbdf9dc6a683aa42b4f40c13a0ea709818001
jessi ortega <ortegabx05@gmail.com>
2019-08-05 22:33:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,13 @@ class Song
 
    attr_accessor :name, :artist, :genre
 
-   ALL = []
+   @@all = []
 
    def initialize(name="", artist="", genre="")
     @name = name
     @artist = artist
     @genre = genre
-    ALL << self
+    @@all << self
   end
 
    def self.all
