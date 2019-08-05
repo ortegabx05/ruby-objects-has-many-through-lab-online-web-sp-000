bf6b9e1f9b334917c0a5e70761eab02a11f2babd
jessi ortega <ortegabx05@gmail.com>
2019-08-05 22:41:54 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,23 +1,24 @@
 class Patient
-  attr_accessor :name, :appointments
-
-   def initialize(name)
-    @name = name
-    @appointments = []
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name=name
+  end
+  
+   def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
   end
 
-   def add_appointment(appointment)
-    @appointments << appointment
-    appointment.patient = self
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
   end
 
-   def appointments
-    @appointments
-  end
-
-   def doctors
-    self.appointments.collect do |appointment|
+  def doctors
+    appointments.map do |appointment|
       appointment.doctor
     end
   end
+  
 end
