fd3c8fb5fc2309c220d0734315904813fb6cf21a
jessi ortega <ortegabx05@gmail.com>
2019-08-05 22:36:06 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,26 +1,11 @@
+
 class Appointment
+  attr_accessor :date, :doctor, :patient
 
-   attr_reader :patient, :doctor, :date
-
-   @@all = []
-
-   def initialize(patient ="", doctor = "", date = "")
-    @patient = patient
-    @doctor = doctor
+   def initialize(date, doctor)
     @date = date
-    @@all << self
-  end
-
-   def self.all
-    @@all
-  end
-
-   def patients
-    self.patient
-  end
-
-   def doctors
-    self.doctor
+    @doctor = doctor
+    doctor.add_appointment(self)
   end
 
  end
